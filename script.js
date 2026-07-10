35e317ca060993d31646f70f610b51d087698f68
mathfews <matheusfcw1010@gmail.com>
2026-07-10 08:02:54 -0300

    feat(tasks): store tasks in application state

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ const new_task_add_btn = document.getElementById("new-task-add-btn")
 const new_task_priority = document.getElementById("new-task-priority")
 const new_task_name = document.getElementById("new-task-name")
 let id = 0
-
+let tasks = []
 new_task_add_btn.addEventListener("click", () => {
+    console.log(tasks)
     const task = {}
     task["id"] = id += 1
     task["title"] = new_task_name.value
     task["priority"] = new_task_priority.value
-    console.log(task)
+    tasks.push(task)
+    new_task_name.value = ""
+    new_task_priority.value = ""
 })
