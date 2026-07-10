563f8fcd4a078715b9fa94f9b53fdea8dc016f26
mathfews <matheusfcw1010@gmail.com>
2026-07-10 08:26:36 -0300

    refactor(ui): extract task rendering into a dedicated function

--- script.js
+++ script.js
@@ -4,28 +4,36 @@ const new_task_name = document.getElementById("new-task-name")
 const tasks_area = document.getElementById("tasks-area")
 let id = 0
 let tasks = []
+
+function render_tasks(tasks) {
+    tasks_area.innerHTML = ""
+    tasks.forEach((task) => {
+        const task_div = document.createElement("div")
+        const task_ul = document.createElement("ul")
+        const task_options = document.createElement("ul")
+        const task_title = document.createElement("li")
+        const task_ID = document.createElement("li")
+        const task_priority = document.createElement("li")
+        task_title.textContent = task.title
+        task_priority.textContent = task.priority
+        task_ID.textContent = task.id
+        task_ul.appendChild(task_title)
+        task_options.appendChild(task_priority)
+        task_options.appendChild(task_ID)
+        task_ul.appendChild(task_options)
+        task_div.appendChild(task_ul)
+        tasks_area.appendChild(task_div)
+    })
+}
+
 new_task_add_btn.addEventListener("click", () => {
     const task_info = {
         id: ++id,
         title: new_task_name.value,
         priority: new_task_priority.value,
     }
-    tasks.push(task_info)
     new_task_name.value = ""
     new_task_priority.value = ""
-    const task = document.createElement("div")
-    const task_ul = document.createElement("ul")
-    const task_options = document.createElement("ul")
-    const task_title = document.createElement("li")
-    const task_ID = document.createElement("li")
-    const task_priority = document.createElement("li")
-    task_title.textContent = task_info.title
-    task_priority.textContent = task_info.priority
-    task_ID.textContent = task_info.id
-    task_ul.appendChild(task_title)
-    task_options.appendChild(task_priority)
-    task_options.appendChild(task_ID)
-    task_ul.appendChild(task_options)
-    task.appendChild(task_ul)
-    tasks_area.appendChild(task)
+    tasks.push(task_info)
+    render_tasks(tasks)
 })
