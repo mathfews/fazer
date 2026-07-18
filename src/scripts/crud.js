f5f34d756e26f9d1473081cc250b1d49a445ee71
mathfews <matheusfcw1010@gmail.com>
2026-07-18 14:30:04 -0300

    refactor: decouple CRUD logic from UI

--- src/scripts/crud.js
+++ src/scripts/crud.js
@@ -1,26 +1,22 @@
 import { getTasks, saveTasks } from "./storage.js"
-import { applyFilters } from "./filters.js"
-import { new_task_name, new_task_priority, update_task_name, update_task_priority, update_task_btn } from "./dom.js"
 
 let editingTask = null
 
-export function addTask() {
+export function addTask(title, priority) {
     let current_tasks = getTasks()
     if (localStorage.getItem("id") === null) {
         localStorage.setItem("id", 0)
     }
     const task_info = {
         id: Number(localStorage.getItem("id")) + 1,
-        title: new_task_name.value,
-        priority: new_task_priority.value,
+        title: title,
+        priority: priority,
         completed: false,
     }
     localStorage.setItem("id", task_info.id)
-    new_task_name.value = ""
-    new_task_priority.value = ""
+
     current_tasks.push(task_info)
     saveTasks(current_tasks)
-    applyFilters()
 }
 
 export function changeTaskState(id) {
@@ -28,39 +24,31 @@ export function changeTaskState(id) {
     const selected_task = selectTask(id, current_tasks)
     selected_task.completed = !selected_task.completed
     saveTasks(current_tasks)
-    applyFilters()
 }
 
 export function updateTask(id) {
     editingTask = selectTask(id)
-    
-    update_task_name.value = ""
-    update_task_priority.value = ""
-    update_task_name.value = editingTask.title
-    update_task_priority.value = editingTask.priority
+
+    return editingTask
 }
 
-export function saveUpdateTask() {
+export function saveUpdateTask(title, priority) {
     if (!editingTask) return
 
     const current_tasks = getTasks()
     const selected_task = selectTask(editingTask.id, current_tasks)
-    selected_task.title = update_task_name.value
-    selected_task.priority = update_task_priority.value
+    selected_task.title = title
+    selected_task.priority = priority
 
     saveTasks(current_tasks)
-
-    applyFilters()
 }
 
 export function deleteTask(id) {
     const current_tasks = getTasks()
     const selected_task = selectTask(id, current_tasks)
     const task_index = current_tasks.indexOf(selected_task)
-    const task_name = selected_task.title
     current_tasks.splice(task_index,1)
     saveTasks(current_tasks)
-    applyFilters()
 }
 
 export function selectTask(id, array=getTasks()) {
