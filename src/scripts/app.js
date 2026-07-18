3e4d325af897cdfbe949c312585f6441842f5546
mathfews <matheusfcw1010@gmail.com>
2026-07-17 23:23:22 -0300

    refactor: migrate app logic to app module

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,18 @@
-import {getTasks, saveTasks} from "./storage.js";
+import { applyFilters } from "./filters.js"
+import { addTask, saveUpdateTask, updateTask } from "./crud.js"
+import { render_tasks } from "./tasks.js"
+import { status_filter, priority_filter, update_task_btn, search_box, new_task_add_btn } from "./dom.js"
+
+let editingTask = null
+
+status_filter.addEventListener("change", applyFilters)
+
+priority_filter.addEventListener("change", applyFilters)
+
+search_box.addEventListener("input", applyFilters)
+
+new_task_add_btn.addEventListener("click", () => addTask())
+
+update_task_btn.addEventListener("click", saveUpdateTask)
+
+render_tasks()
