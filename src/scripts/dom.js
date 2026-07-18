0f7756312539570860b2e767f09ebe1bd83a97b7
mathfews <matheusfcw1010@gmail.com>
2026-07-17 22:36:57 -0300

    refactor: centralize DOM selectors

---
+++ src/scripts/dom.js
@@ -0,0 +1,17 @@
+
+export const new_task_add_btn = document.getElementById("new-task-add-btn")
+export const new_task_name = document.getElementById("new-task-name")
+export const new_task_priority = document.getElementById("new-task-priority")
+
+
+export const tasks_area = document.getElementById("tasks-area")
+
+
+export const update_task_btn = document.getElementById("update-task-btn")
+export const update_task_name = document.getElementById("update-task-name")
+export const update_task_priority = document.getElementById("update-task-priority")
+
+
+export const search_box = document.getElementById("search-box")
+export const status_filter = document.getElementById("status-filter")
+export const priority_filter = document.getElementById("priority-filter")
