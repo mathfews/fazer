11dc9584071f6fd1ee8ebd9783f791564301ab39
mathfews <matheusfcw1010@gmail.com>
2026-07-20 19:59:21 -0300

    refactor: remove frontend task ID generation

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from itertools import count
+
+id_generator = count(start=0)
 
 class Task(BaseModel):
+    id: int | None = 0
     title: str
     priority: str
     completed: bool | None = False
@@ -12,6 +16,7 @@ db = []
 
 @app.post("/tasks/")
 async def add_task(task: Task):
+    task.id = next(id_generator)
     db.append(task)
 
 @app.get("/tasks/")
